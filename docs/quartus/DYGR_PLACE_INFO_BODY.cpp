8ae144426c90ec45aeff1206a0aa65cc80d86f13
Dan Ravensloft <dan.ravensloft@gmail.com>
2020-03-17 23:49:52 +0000

    More progress on DYGR_PLACE_INFO_BODY

--- docs/quartus/DYGR_PLACE_INFO_BODY.cpp
+++ docs/quartus/DYGR_PLACE_INFO_BODY.cpp
@@ -12,7 +12,17 @@ struct DYGR_PLACE_INFO_BODY {
     std::vector<UNKNOWN5*> m_phys_impl_list;
     std::vector<UNKNOWN6>  m_place_block_type_info;
     std::vector<UNKNOWN7*> m_blk_type_info_lookup;
-    std::unordered_map<std::string, UNKNOWN8>
+    std::unordered_map<std::string, UNKNOWN8> m_user_string_to_place_block_location_hash;
+    uint32_t               m_nx;
+    uint32_t               m_ny;
+    std::string            m_blk_list_source_file_string;
+    std::string            m_blk_list_id_string;
+    std::string            m_blk_list_chip_id_string;
+    std::string            m_pin_table_version_string;
+    std::vector<std::vector<uint32_t>> m_loc_properties;
+    UNKNOWN9               m_clock_division_info;
+    uint32_t               m_third_dimension_enum;
+    UNKNOWN10              m_back_bias_region_info;
 };
 
 struct UNKNOWN1;
@@ -44,4 +54,15 @@ struct UNKNOWN8 {
     uint16_t m_x_loc;
     uint16_t m_y_loc;
     uint16_t m_z_loc;
-}
+};
+
+struct UNKNOWN9 {
+    uint32_t m_division_vec;
+};
+
+struct UNKNOWN10 {
+    uint32_t m_nx;
+    uint32_t m_ny;
+    int32_t  m_max_tile_id;
+    uint32_t m_tile_ids;
+};
