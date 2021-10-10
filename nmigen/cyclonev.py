ce696a117531c992f70fb729b40d12c896de134d
Olivier Galibert <galibert@pobox.com>
2021-10-10 18:22:11 +0200

    Start of nmigen cyclonev specific support

---
+++ nmigen/cyclonev.py
@@ -0,0 +1,12 @@
+from nmigen import *
+
+class hps_general_purpose(Elaboratable):
+    def __init__(self):
+        self.o_gp_out = Signal(32)
+        self.i_gp_in = Signal(32)
+
+    def elaborate(self, platform):
+        m = Module()
+        m.submodules.i = Instance("cyclonev_hps_interface_mpu_general_purpose", i_gp_in = self.i_gp_in, o_gp_out = self.o_gp_out)
+        return m
+
